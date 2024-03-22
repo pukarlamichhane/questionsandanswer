4f66f063e4931a3c60ffbdec2dd5bb831da773a4
pukarlamichhane <pukarlamichhane567@gmail.com>
2024-03-22 14:40:27 +0545

    add cloudinary

---
+++ backend/helpers/utils.js
@@ -0,0 +1,24 @@
+const crypto = require("crypto");
+const cloudinary=require("cloudinary")
+// Arrow function to generate a random number between min (inclusive) and max (exclusive)
+const generateRandomNumber = () => {
+    return crypto.randomInt(0, 1000000);
+}
+
+const uploadImageAndUpdateURL = async (image) => {
+    try {
+        // Upload image to Cloudinary
+        const uploadedResponse = await cloudinary.uploader.upload(image);
+        
+        // Update image URL
+        return uploadedResponse.secure_url;
+    } catch (error) {
+        // Handle any errors that occur during upload
+        console.error("Error uploading image:", error);
+        throw error; // Re-throw the error for the caller to handle
+    }
+};
+
+
+// Export the function
+module.exports = {generateRandomNumber,uploadImageAndUpdateURL};
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cloudinary=require('cloudinary')
 const connectDB = require('./db/connectDB');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const useroutes  = require('./routes/userRoutes');
 const productroutes  = require('./routes/productRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -20,6 +22,7 @@ cloudinary.config({
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 
 // Correct the path prefix for user routes
 app.use("/api/user", useroutes);
