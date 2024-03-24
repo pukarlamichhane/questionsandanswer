f40b5c8087d6249829853322d2c58cccd64936fe
pukarlamichhane <pukarlamichhane567@gmail.com>
2024-03-24 18:12:09 +0545

    add cors to allow all the origin to get data from the api

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,31 @@
 const express = require('express');
-const cloudinary=require('cloudinary')
+const cloudinary = require('cloudinary');
 const connectDB = require('./db/connectDB');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const useroutes  = require('./routes/userRoutes');
-const productroutes  = require('./routes/productRoutes');
-
+const cors = require('cors'); // Import CORS middleware
+const useroutes = require('./routes/userRoutes');
+const productroutes = require('./routes/productRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
-app.use(cookieParser()); // Use cookie-parser middleware
+app.use(cookieParser());
+app.use(cors()); // Allow all origins
 
 connectDB();
 
-//add cloudinary config 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
-// Correct the path prefix for user routes
 app.use("/api/user", useroutes);
-app.use("/api/product",productroutes);
+app.use("/api/product", productroutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at port :${PORT}`);
+    console.log(`Server is listening at port :${PORT}`);
 });
