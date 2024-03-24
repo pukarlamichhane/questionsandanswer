799b2ce89c475dcde19e823222b4a3d3d7ac77df
pukarlamichhane <pukarlamichhane567@gmail.com>
2024-03-24 15:21:43 +0545

    make responsive of signup page

--- frontend/src/page/Signup.jsx
+++ frontend/src/page/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+//import backgroundImage from './background.jpg'; // Import your background image
 
 const Signup = () => {
   const [email, setEmail] = useState('');
@@ -20,8 +21,11 @@ const Signup = () => {
   };
 
   return (
-    <div className='w-full h-screen flex justify-center items-center '>
-      <div className='max-w-md w-full px-4 border-2 border-gray-600 rounded bg-white'>
+    <div
+      className='w-full min-h-screen flex justify-center items-center bg-cover bg-center'
+      style={{ backgroundImage: `url(https://media.cntraveler.com/photos/56420d2496771ce632e3df6a/master/pass/sneakers-tout.jpg)` }}
+    >
+      <div className='max-w-md w-full px-4 border-2 border-gray-800 rounded bg-white'>
         <h1 className='text-3xl font-bold text-center py-4 text-black'>Sign up</h1>
         <form className='mt-8 px-4 py-6' onSubmit={handleSubmit}>
           <input
@@ -54,7 +58,7 @@ const Signup = () => {
             )}
           </div>
           <button type='submit' className='bg-red-600 py-3 my-6 rounded w-full text-white font-bold'>
-            Sign up
+            Login
           </button>
           <div className='flex justify-between items-center text-sm'>
             <label className='flex items-center text-black'>
@@ -62,7 +66,7 @@ const Signup = () => {
               <span>Remember me</span>
             </label>
             <span>
-              <Link to="/login" className='text-gray-500'>Already have an account?</Link>
+              <Link to="/signup" className='text-gray-500'>Forget password</Link>
             </span>
           </div>
         </form>
