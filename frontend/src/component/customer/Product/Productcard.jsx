fb1c8e93bddd3c82f552dbd3eaa0c060d320e31d
pukarlamichhane <pukarlamichhane567@gmail.com>
2024-04-29 19:12:47 +0545

    fix the error in the product card

--- frontend/src/component/customer/Product/Productcard.jsx
+++ frontend/src/component/customer/Product/Productcard.jsx
@@ -1,17 +1,16 @@
-// eslint-disable-next-line react/prop-types
+/* eslint-disable react/prop-types */
 const Productcard = ({ image, name }) => {
   return (
-    <div className="w-[15rem] border  m-3 transition-all cursor-pointer">
-      <div className="h-[20rem">
+    <div className="w-60 h-72 border rounded-lg m-3 transition-all duration-300 hover:shadow-lg cursor-pointer">
+      <div className="h-3/4 rounded-t-lg overflow-hidden">
         <img
-          className="h-full w-full object-cover object-left-top"
+          className="h-full w-full object-cover object-center"
           src={image}
+          alt={name}
         />
       </div>
-      <div className="textPart  bg-white p-3">
-        <div>
-          <p className="font-bold text-center opacity-100">{name}</p>
-        </div>
+      <div className="bg-white p-3 rounded-b-lg flex justify-center items-center">
+        <p className="font-bold text-center opacity-100">{name}</p>
       </div>
     </div>
   );
